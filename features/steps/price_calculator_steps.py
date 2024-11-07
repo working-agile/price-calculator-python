cfb0c1ff1b778247cb2749aeac37134595c36d69
Nathan Silva Ferreira <116688602+nathansilva009@users.noreply.github.com>
2024-11-07 16:02:14 +0000

    setup for first scenario

---
+++ features/steps/price_calculator_steps.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+
+@given('a context')
+def step_given_a_context():
+    # Write code here that turns the phrase above into concrete actions
+    raise NotImplementedError("Pending implementation")
+
+@when('something happens')
+def step_when_something_happens():
+    # Write code here that turns the phrase above into concrete actions
+    raise NotImplementedError("Pending implementation")
+
+@then('something should be different now...')
+def step_then_something_should_be_different_now():
+    # Write code here that turns the phrase above into concrete actions
+    raise NotImplementedError("Pending implementation")
